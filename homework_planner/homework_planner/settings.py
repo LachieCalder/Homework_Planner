4a561f385358ba1e7a4b02756b926a85f1662355
Lachie Calder <lachiecalder@outlook.com>
2013-11-24 08:47:55 +1300

    use base html template with Twitter Bootstrap

--- homework_planner/homework_planner/settings.py
+++ homework_planner/homework_planner/settings.py
@@ -80,7 +80,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, "static"),	
+) 
 
 TEMPLATE_DIRS = (
 	BASE_DIR + '/templates/',	
