02ee080903ff7cb78fff50a3208e26961abdf229
Lachie Calder <lachiecalder@outlook.com>
2013-11-24 07:49:32 +1300

    add models; sync database

--- homework_planner/assignments/models.py
+++ homework_planner/assignments/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Assignment(models.Model):
+  due = models.DateField()
+  subject = models.ForeignKey('Subject')
+  description = models.CharField(max_length=200)
+  done = models.BooleanField(default=False)
+
+  def __unicode__(self):
+	return self.description
+
+class Subject(models.Model):
+  name = models.CharField(max_length=30)
+
+  def __unicode__(self):
+	return self.name
