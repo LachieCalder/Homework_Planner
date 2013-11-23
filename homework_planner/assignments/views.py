a7030edfe2e8f30e2131085af2d68076cc719f40
Lachie Calder <lachiecalder@outlook.com>
2013-11-24 08:02:12 +1300

    create assignments view

--- homework_planner/assignments/views.py
+++ homework_planner/assignments/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Assignment, Subject
 
 # Create your views here.
+
+class AssignmentView(ListView):
+  model = Assignment
+  context_object_name = 'assignment_list'
